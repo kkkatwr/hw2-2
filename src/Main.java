191ce61931be6264f39292479b4bd7c42c0b2540
kkkatwr <Katyad1318@gmail.com>
2024-03-28 00:12:45 +0500

    gg

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int a = 200; //начальный баланс клиента
-        int b = 1600; //сумма пополнения
-        int d = a + b;
+        int StartBalance = 200; //начальный баланс клиента
+        int RefillSum = 1600; //сумма пополнения
+        int TotalBalance = StartBalance + RefillSum;
 
-        if (b > 1000) {
-            int f = b / 100;
-            System.out.println("Начислено бонусов = " + (f) + ". Итоговая сумма с учетом бонусов на балансе = " + (d + f));
+        if (RefillSum > 1000) {
+            int Bonus = RefillSum / 100;
+            System.out.println("Начислено бонусов = " + (Bonus) + ". Итоговая сумма с учетом бонусов на балансе = " + (TotalBalance + Bonus));
         } else {
-            System.out.println("Начислено бонусов = 0 " + "Итоговая сумма на балансе = " + (d));
+            System.out.println("Начислено бонусов = 0 " + "Итоговая сумма на балансе = " + (TotalBalance));
         }
 
     }
